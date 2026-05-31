bbc3df3b16b436a420222df4de5a9e8488aeda11
xmple <146173165+Gamomama2004@users.noreply.github.com>
2026-06-01 02:39:43 +0300

    Update server.py

--- spice_monitor/server.py
+++ spice_monitor/server.py
@@ -2063,5 +2063,6 @@ class HTTPServer:
             client.close()
 
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get('PORT', 8080))
     HTTPServer(host='0.0.0.0', port=port).run()
